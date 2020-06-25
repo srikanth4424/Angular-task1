c36b5e2c26df0b32b0d0b922c6b7564f708fdd04
srikanth4424 <samsrikanth007@gmail.com>
2020-06-25 11:43:18 +0530

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UsersListComponent } from './users/users-list/users-list.component';
 import { UsersDetailComponent } from './users/users-detail/users-detail.component';
 import { ReposComponent } from './repos/repos.component';
 import { SearchUsersComponent } from './users/search-users/search-users.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoaderComponent } from './layouts/loader/loader.component';
 
 const routes: Routes = [{
     path: '',
@@ -27,12 +25,10 @@ const routes: Routes = [{
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     UsersListComponent,
     UsersDetailComponent,
     ReposComponent,
-    SearchUsersComponent,
-    LoaderComponent
+    SearchUsersComponent
   ],
   imports: [
     BrowserModule,
